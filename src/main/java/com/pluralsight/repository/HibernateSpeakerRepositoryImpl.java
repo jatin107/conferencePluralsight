cf12ce84d1b95f5a94e45fc8f771a4630a9ba145
jatin goyal <jatingoyal100@gmail.com>
2021-04-09 13:10:46 +0530

    Project without Spring

---
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -0,0 +1,21 @@
+package com.pluralsight.repository;
+
+import com.pluralsight.model.Speaker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Override
+    public List<Speaker> findAll() {
+        List<Speaker> speakers = new ArrayList<>();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("Jatin");
+        speaker.setLastName("Goyal");
+        speakers.add(speaker);
+        return speakers;
+    }
+
+
+}
