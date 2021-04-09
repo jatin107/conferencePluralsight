6ce4a5e369ee8063cfa4804abcce15deff74f098
jatin goyal <jatingoyal100@gmail.com>
2021-04-09 17:26:46 +0530

    Spring Configuration using java Task 1. Change in pom file 2. Created AppConfig for Application context and DI. 3. Used Setter Injection for SpeakerRepository. 4. In Application get Bean using the ApplicationContext.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,16 @@
 import com.pluralsight.model.Speaker;
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
-    public static void main(String str[])
-    {
-        SpeakerService service= new SpeakerServiceImpl();
+
+    public static void main(String str[]) {
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
+        //SpeakerService service = new SpeakerServiceImpl();
         System.out.println(service.findAll().get(0).getFirstName());
     }
 }
