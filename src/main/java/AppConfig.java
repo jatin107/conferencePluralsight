3bc5d1028a4dd7156681903c4bd44eb202dec87a
jatin goyal <jatingoyal100@gmail.com>
2021-04-09 20:43:51 +0530

    Spring Configuration using java Task 1.Prototype Java Config

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -10,7 +10,8 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class AppConfig {
     @Bean(name = "speakerService")
-    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)//singleton
+   // @Scope(value = BeanDefinition.SCOPE_PROTOTYPE)//prototype
     //@Scope(value="singleton")//
     public SpeakerService getSpeakerService() {
         SpeakerServiceImpl service=new SpeakerServiceImpl(getSpeakerRepository());//Constructor Injection
