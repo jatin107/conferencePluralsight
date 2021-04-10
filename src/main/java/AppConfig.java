a5f23cef6444b6f025f465fd5b38133f7aec2dfc
jatin goyal <jatingoyal100@gmail.com>
2021-04-10 06:39:19 +0530

    Spring Configuration using java Task 1.Component Scan

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,12 +4,14 @@ import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan({"com.pluralsight"})
 public class AppConfig {
-    @Bean(name = "speakerService")
+    /*@Bean(name = "speakerService")
     @Scope(value = BeanDefinition.SCOPE_SINGLETON)//singleton
    // @Scope(value = BeanDefinition.SCOPE_PROTOTYPE)//prototype
     //@Scope(value="singleton")//
@@ -24,5 +26,5 @@ public class AppConfig {
     @Bean(name = "speakerRepository")
     public SpeakerRepository getSpeakerRepository() {
         return new HibernateSpeakerRepositoryImpl();
-    }
+    }*/
 }
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -4,9 +4,11 @@ import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository;
